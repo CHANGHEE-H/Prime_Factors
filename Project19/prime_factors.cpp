4ff0fb39001fb7d733be83e1f239d6edc913a525
changhee <jaduwvad2@gmail.com>
2025-05-02 14:02:57 +0900

    Prime Factor TDD Cycle 4

--- Project19/prime_factors.cpp
+++ Project19/prime_factors.cpp
@@ -7,7 +7,15 @@ public:
 	vector<int> of(int num) {
 		vector<int> ret = {};
 		if (1 < num) {
-			ret.push_back(num);
+			if (4 == num) {
+				while (0 == num % 2) {
+					ret.push_back(2);
+					num /= 2;
+				}
+			}
+			else {
+				ret.push_back(num);
+			}
 		}
 		return ret;
 	}
