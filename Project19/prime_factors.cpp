acef54717a5530043a37edeb38c2dd4874f863b1
changhee <jaduwvad2@gmail.com>
2025-05-02 14:11:24 +0900

    Prime Factor TDD Cycle 7

--- Project19/prime_factors.cpp
+++ Project19/prime_factors.cpp
@@ -6,19 +6,13 @@ class PrimeFactor {
 public:
 	vector<int> of(int num) {
 		vector<int> ret = {};
-		if (1 < num) {
-			if (4 == num || 6 == num || 9 == num) {
-				for (int divisor = 2; 1 < num; divisor++) {
-					while (0 == num % divisor) {
-						ret.push_back(divisor);
-						num /= divisor;
-					}
-				}
-			}
-			else {
-				ret.push_back(num);
+		for (int divisor = 2; 1 < num; divisor++) {
+			while (0 == num % divisor) {
+				ret.push_back(divisor);
+				num /= divisor;
 			}
 		}
+		
 		return ret;
 	}
 };
--- Project19/prime_factors_test.cpp
+++ Project19/prime_factors_test.cpp
@@ -36,3 +36,8 @@ TEST_F(PrimeFixture, OF9) {
 	expected = { 3, 3 };
 	EXPECT_EQ(expected, prime_factor.of(9));
 }
+
+TEST_F(PrimeFixture, OF12) {
+	expected = { 2, 2, 3 };
+	EXPECT_EQ(expected, prime_factor.of(12));
+}
